8c5e89e827dc81791c16a4ba86b69f43ba2acaad
SharyZ <onshariiit@gmail.com>
2022-03-23 13:33:45 +0500

    user FullscreenLayout in NotFound page

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,3 +1,11 @@
+import { FullscreenLayout } from "../components";
+
 export const Notfound = () => {
-  return <div>404 - Not Found</div>;
+  return (
+    <FullscreenLayout>
+      <div className="mx-auto flex min-h-screen flex-col items-center justify-center">
+        <h1 className="text-center">404 - Not Found.</h1>
+      </div>
+    </FullscreenLayout>
+  );
 };
